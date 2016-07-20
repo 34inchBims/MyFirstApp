ffeb5c3fecc762db7aaabe965b591e3f53153e5b
34inchBims <bsimsee@gmail.com>
2016-07-19 23:28:11 -0400

    initial commit

---
+++ app/src/main/java/com/example/mcleod/myfirstapp/DisplayMessageActivty.java
@@ -0,0 +1,13 @@
+package com.example.mcleod.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message_activty);
+    }
+}
